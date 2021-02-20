09f7a6cdb7e495a26283fc8319c19ea3dcbd08b3
sjasminetya <shaulajasminetya22@gmail.com>
2021-02-20 15:42:09 +0700

    todos login

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styles from './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className={styles['todo-login']}>
+              <h4>Todos Login</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
