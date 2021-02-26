4de4a455d335dc0189826529b47c9903e049a04c
sjasminetya <shaulajasminetya22@gmail.com>
2021-02-26 22:40:06 +0700

    ubah tampilan admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,10 +1,15 @@
-import React, {useEffect, useState} from 'react'
-import styles from './Admin.module.css'
-import {useHistory} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {getAllLabel, addLabel, deleteLabel} from '../../configs/redux/actions'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllLabel, addLabel, deleteLabel } from '../../configs/redux/actions'
 import Input from '../../component/base/Input/Input'
-import Button from '../../component/base/Button/Button'
+import { Toast, ToastBody, ToastHeader, Container, Spinner, Button } from "reactstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faEdit,
+    faTrash,
+    faPlus
+} from "@fortawesome/free-solid-svg-icons"
 
 export default function Admin() {
     const [label, setLabel] = useState('')
@@ -37,35 +42,61 @@ export default function Admin() {
     const handleDeleteLabel = (id) => {
         dispatch(deleteLabel(id))
     }
-    
+
     useEffect(() => {
         dispatch(getAllLabel())
-    }, [])
+    }, [dispatch])
 
     return (
         <div>
-            <section>
-                <div className={styles.label}>
-                    <div className={styles.text}>
-                        <h4>Add label</h4>
+            <div className="p-3 bg-dark my-2 mt-5 rounded" style={{ width: '480px', margin: 'auto', display: 'flex' }}>
+                <Input value={label} type='text' id='label' placeholder='type label' onChange={handleChangeLabel} />
+                <Button onClick={handleAddLabel} className="mr-2 mt-4" style={{ width: 'max-content' }}>
+                    <FontAwesomeIcon icon={faPlus} /> Add New Label
+                </Button>
+            </div>
+            <Container>
+                {labelState ? (
+                    <div className="p-3 bg-dark my-2 mt-5 rounded" style={{ width: 'max-content', margin: 'auto' }}>
+                        <h3 style={{ color: '#FFFFFF' }} className="text-center">List Label</h3>
+                        {labelState.map((item) => (
+                            <div style={{display: 'flex'}}>
+                                <Toast key={item.id} onClick={() => goTask(item.id)} className="mt-3 mb-3 ml-5 mr-5" style={{ width: '220px', cursor: 'pointer' }}>
+                                    {item.label === 'important' ? (
+                                        <div>
+                                            <ToastHeader icon="danger">
+                                                {item.label}
+                                            </ToastHeader>
+                                            <ToastBody>
+                                                {item.desc}
+                                            </ToastBody>
+                                        </div>
+                                    ) : (
+                                            <div>
+                                                <ToastHeader icon="info">
+                                                    {item.label}
+                                                </ToastHeader>
+                                                <ToastBody>
+                                                    {item.desc}
+                                                </ToastBody>
+                                            </div>
+                                        )}
+                                </Toast>
+                                <Button color="info" className="mr-2 mt-4" onClick={() => goEditLabel(item.id)}>
+                                    <FontAwesomeIcon icon={faEdit} /> Edit
+                                </Button>
+                                <Button color="info" style={{width: '120px'}} className="mr-2 mt-4" onClick={() => handleDeleteLabel(item.id)}>
+                                    <FontAwesomeIcon icon={faTrash} /> Delete
+                                </Button>
+                            </div>
+                        ))}
                     </div>
-                    <form>
-                        <Input value={label} className={styles['input-label']} type='text' id='label' placeholder='add label' onChange={handleChangeLabel} />
-                        <Button title='Add' onClick={handleAddLabel} />
-                    </form>
-                    {labelState.map((item) => (
-                        <div className={styles['item-label']}>
-                            <div className={styles['card-label']} key={item.id} onClick={() => goTask(item.id)}>
-                                <span>{item.label}</span>
-                            </div>
-                            <div className={styles['group-btn']}>
-                                <Button title='Edit' onClick={() => goEditLabel(item.id)} />
-                                <Button title='Delete' onClick={() => handleDeleteLabel(item.id)} />
-                            </div>
+                ) : (
+                        <div className="text-center p-3 bg-dark my-2 rounded" style={{width: '520px', margin: 'auto'}}>
+                            <Spinner color="primary" />
                         </div>
-                    ))}
-                </div>
-            </section>
+                    )}
+            </Container>
         </div>
     )
 }
